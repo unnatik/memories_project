0ae16a52a5b913b9cd1bb65e9f98f42bd4becacc
Unnati <unnatikolhe@gmail.com>
2021-05-22 09:53:23 +0530

    add reducer, actions

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 import memories from "./images/memories.png";
 import Form from "./components/Form/Form";
 import Posts from "./components/Posts/Posts";
 import useStyles from "./styles";
+import { getPosts } from "./actions/posts";
+import { useDispatch } from "react-redux";
 
 const App = () => {
-    const classes = useStyles();
+  const classes = useStyles();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
+
   return (
     <Container maxwidth="lg">
       <AppBar className={classes.appBar} position="static" color="inherit">
         <Typography className={classes.heading} variant="h2" align="center">
           Memories
         </Typography>
-        <img className={classes.image} src={memories} alt="memories" height="60" />
+        <img
+          className={classes.image}
+          src={memories}
+          alt="memories"
+          height="60"
+        />
       </AppBar>
       <Grow in>
         <Grid
